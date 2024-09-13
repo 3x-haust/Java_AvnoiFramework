8df18dd1bc92d9c49dbb4a6f1900ff3ff6a5bf91
3x-haust <lyusungyun@icloud.com>
2024-09-13 09:15:32 +0900

    feat: All, Head,Options annotations

--- src/main/java/io/github/_3xhaust/annotations/types/Head.java
+++ src/main/java/io/github/_3xhaust/annotations/types/Head.java
@@ -1,4 +1,12 @@
 package io.github._3xhaust.annotations.types;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
 public @interface Head {
+    String value() default "";
 }
